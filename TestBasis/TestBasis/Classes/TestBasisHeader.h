3ae2c4b4e0cde9675170904194f6c66d72a87226
大苏Andi <szb2323@163.com>
2019-07-17 13:58:44 +0800

    save

--- TestBasis/TestBasis/Classes/TestBasisHeader.h
+++ TestBasis/TestBasis/Classes/TestBasisHeader.h
@@ -16,19 +16,6 @@
 #import <MGJRouter.h>
 #import <CTMediator.h>
 
-
-
-/** 组件 bundle */
-#define BUNDLE_BUSINESS_MOUDLE [NSBundle BundleWithPath:@"/TestBasis.bundle" withClass:[self class]]//基础组件路径
-
-#define BUNDLE_TESTA_MOUDLE [NSBundle BundleWithPath:@"/TestA.bundle" withClass:[self class]]//TestA组件路径
-
-#define BUNDLE_TESTB_MOUDLE [NSBundle BundleWithPath:@"/TestB.bundle" withClass:[self class]]//TestB组件路径
-
-#define BUNDLE_TESTC_MOUDLE [NSBundle BundleWithPath:@"/TestC.bundle" withClass:[self class]]//TestC组件路径
-
-
-
 //CTMediator_Category
 #import "CTMediator+TestA.h"
 #import "CTMediator+TestB.h"
@@ -42,5 +29,14 @@
 
 #define KEYWITHNavigation @"navigationVC"
 #define KEYWITHViewController @"ViewController"
+
+/** 组件 bundle */
+#define BUNDLE_BUSINESS_MOUDLE [NSBundle BundleWithPath:@"/TestBasis.bundle" withClass:[self class]]//基础组件路径
+
+#define BUNDLE_TESTA_MOUDLE [NSBundle BundleWithPath:@"/TestA.bundle" withClass:[self class]]//TestA组件路径
+
+#define BUNDLE_TESTB_MOUDLE [NSBundle BundleWithPath:@"/TestB.bundle" withClass:[self class]]//TestB组件路径
+
+#define BUNDLE_TESTC_MOUDLE [NSBundle BundleWithPath:@"/TestC.bundle" withClass:[self class]]//TestC组件路径
 
 #endif /* TestBasisHeader_h */
